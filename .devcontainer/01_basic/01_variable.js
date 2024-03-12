efdfe8834d6fffeb9d2b071675794256ffac2ed9
Asif khan <80121417+khanasif3386@users.noreply.github.com>
2024-03-12 08:04:13 +0000

    variable and datatypes

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "asifkh3322@gmailc.com"
+var accountPassword = "12345"
+accountICity = "jaipur"
+
+// accountId = 2  // not allowed
+
+accountEmail = "asd@hi"
+accountPassword = "098765"
+// accountICity = bengaluru
+
+console.log(accountId);
+console.log([accountId, accountEmail, accountPassword, accountICity]);
---
+++ .devcontainer/01_basic/02_dataType.js
@@ -0,0 +1,20 @@
+console.log("asif")
+
+let name = "asif"
+let age = 24
+let isLoggedIn = false
+let state;
+let stateOne = null
+
+
+
+// number => 2 to power 53
+// BigInt 
+// String => "" 
+// Boolean => true/false
+// null => standalone value 
+// undefined  
+// Symbol => unique
+
+
+// Object
